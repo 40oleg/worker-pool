d0a232ea64e0dfe4cdc43152fb33dbecd6e069c3
hanahana <enigma597@mail.ru>
2022-10-23 01:06:18 +0300

    worker.js

---
+++ worker.js
@@ -0,0 +1,7 @@
+import { parentPort } from 'worker_threads'
+import { FunctionParse } from "./FunctionSerializer.js";
+
+parentPort.on('message', (postData) => {
+    const parsedFunction = FunctionParse(postData.func);
+    parentPort.postMessage(parsedFunction(...postData.arguments));
+})
